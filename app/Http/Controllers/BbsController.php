59e95a80372ce03fc84723915139eb979ada4d03
Sergei Rakitin <serjo-66@mail.ru>
2023-03-18 21:41:35 +0400

    Добавил модель, реализовал метод detail вывод отдельного объявления

--- app/Http/Controllers/BbsController.php
+++ app/Http/Controllers/BbsController.php
@@ -2,13 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bb;
 use Illuminate\Http\Request;
 
 class BbsController extends Controller
 {
     public function index()
     {
-        return response('Здесть будет перечень наших объявлений')
-                        ->header('Content-Type', 'text/plain');
+        $bbs = Bb::latest()->get();
+        $s = "Объявления\r\n\r\n";
+        foreach ($bbs as $bb) {
+            $s .= $bb->title . "\r\n";
+            $s .= $bb->price . " руб.\r\n";
+            $s .= "\r\n";
+        }
+        return response($s)->header('Content-Type', 'text/plain');
+    }
+
+    public function detail(Bb $bb)
+    {
+        $s = $bb->title . "\r\n\r\n";
+        $s .= $bb->content . "\r\n";
+        $s .= $bb->price . " руб.\r\n";
+
+        return response($s)->header('Content-Type', 'text/plain');
     }
 }
