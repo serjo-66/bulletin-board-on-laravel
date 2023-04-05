7ed0a81352ca8da14c366920c14ff4eabfd5ddca
Sergei Rakitin <serjo-66@mail.ru>
2023-04-05 17:03:13 +0400

    Добавил шаблоны, лого

---
+++ resources/views/detail.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.base')
+
+@section('title', $bb->title)
+
+@section('main')
+    <h2>{{ $bb->title }}</h2>
+    <p>{{ $bb->content }}</p>
+    <p>{{ $bb->price }} руб.</p>
+    <p><a href="{{ route('index') }}">На главную</a></p>
+@endsection
