081de8006824a46b85d3bc0e054a9278ee53e543
Sergei Rakitin <serjo-66@mail.ru>
2023-05-02 22:53:28 +0400

    Получение сведений о текущем пользователе

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -24,18 +24,22 @@
     <nav class="navbar navbar-light bg-light">
         <a href="{{ route('index') }}"
            class="navbar-brand mr-auto ">Главная</a>
-        <a href="{{ route('register') }}"
-           class="nav-item nav-link ">Регистрация</a>
-        <a href="{{ route('login') }}"
-           class="nav-item nav-link">Вход</a>
-        <a href="{{ route('home') }}"
-           class="nav-item nav-link">Мои объявления</a>
-        <form action="{{ route('logout') }}" method="POST"
-              class="form-inline">
-            @csrf
-            <input type="submit" class="btn btn-danger"
-                   value="Выход">
-        </form>
+        @guest()
+            <a href="{{ route('register') }}"
+               class="nav-item nav-link ">Регистрация</a>
+            <a href="{{ route('login') }}"
+               class="nav-item nav-link">Вход</a>
+        @endguest
+        @auth()
+            <a href="{{ route('home') }}"
+               class="nav-item nav-link">Мои объявления</a>
+            <form action="{{ route('logout') }}" method="POST"
+                  class="form-inline">
+                @csrf
+                <input type="submit" class="btn btn-danger"
+                       value="Выход">
+            </form>
+        @endauth
     </nav>
     <h1 class="my-3 text-center">Объявления</h1>
     @yield('main')
